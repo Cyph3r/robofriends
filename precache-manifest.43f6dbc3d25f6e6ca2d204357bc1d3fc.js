ef9245bfc7190b8b572c8cd2f9ce79abac5460aa
Derek <derekj77@gmail.com>
2019-11-16 10:23:25 -0600

    Updates

---
+++ precache-manifest.43f6dbc3d25f6e6ca2d204357bc1d3fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6734cd3614b6d2ef263eaf926feb1d7a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "9f76b6f690807f2b463d",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "65371d1d08913cd64859",
+    "url": "/robofriends/static/css/main.6aa50de9.chunk.css"
+  },
+  {
+    "revision": "9f76b6f690807f2b463d",
+    "url": "/robofriends/static/js/2.ebc16967.chunk.js"
+  },
+  {
+    "revision": "65371d1d08913cd64859",
+    "url": "/robofriends/static/js/main.211e0b8d.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
